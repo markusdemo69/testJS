4a1708852fca355bcf0f28ccadad93e5f4458f4b
Ivan_K <ivan.kovalyov@doxy.me>
2020-12-07 14:48:31 +0200

    fisrst commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+console.log('arr' + ' - obj')
+console.log(4 + ' - obj')
+
+let inct = 10,
+    decr = 10;
+//df
+
+//inct++;
+//decr--;
+
+console.log(++inct)
+console.log(--decr)
+
+
+console.log(5%3)
+
+
+console.log(2*4 != 8)
+
+const isCheked = false,
+    isClose = false;
+
+console.log(isCheked || !isClose)
+
+console.log(2 + 2 * 2 != 8 )
