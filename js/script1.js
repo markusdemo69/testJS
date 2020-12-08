fe9fcd9f132a678ae9fadade6d5ae08986f3dbd0
Ivan_K <ivan.kovalyov@doxy.me>
2020-12-08 11:08:24 +0200

    условия

---
+++ js/script1.js
@@ -0,0 +1,17 @@
+"use strict"
+
+if (4 == 5) {
+    console.log('ok')
+} else{
+    console.log('error')
+}
+
+const num = 50;
+
+if (num < 49) {
+    console.log('error')
+} else if (num > 100) {
+    console.log('mnogo')
+} else {
+    console.log('ok')
+}
